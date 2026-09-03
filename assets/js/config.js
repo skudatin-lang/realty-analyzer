d5e6fa596cae09a59eab281ed5f6ba27682d4c43
skudatin-lang <skudatin@gmail.com>
2026-09-03 12:33:00 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-window.REALTY_ANALYZER_CONFIG = { API_BASE_URL: "https://YOUR_FUNCTION_ID.apigw.yandexcloud.net" };
+window.REALTY_ANALYZER_CONFIG = { API_BASE_URL: "https://d5dfod5gq4chs32eu2b1.g4vq2kuy.apigw.yandexcloud.net" };
